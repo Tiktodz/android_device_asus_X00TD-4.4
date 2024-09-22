b5e93ea48f520b9a18055f0f7954e08d5cb386fe
Meisie Widuri <ewprjkt@proton.me>
2024-09-22 13:04:20 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,9 +6,9 @@ rm -rf kernel/asus/sdm660/KernelSU/scripts
 rm -rf kernel/asus/sdm660/KernelSU/docs
 
 rm -rf device/asus/sdm660-common
-rm -rf device/asus/X00TD
-git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common-4.4 device/asus/sdm660-common
-git clone --depth=1 https://github.com/Tiktodz/android_device_asus_X00TD-4.4 device/asus/X00TD
+#rm -rf device/asus/X00TD
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common-4.4 -b vic device/asus/sdm660-common
+#git clone --depth=1 https://github.com/Tiktodz/android_device_asus_X00TD-4.4 device/asus/X00TD
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus-4.4 vendor/asus
