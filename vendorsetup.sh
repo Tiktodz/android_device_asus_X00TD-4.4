588a19e2031816049f73d1e1c66cbd32fa85278c
Tiktodz <dotkit@electrowizard.me>
2024-10-15 16:28:16 +0000

    X00TD: Matrixx BRINGUP

---
+++ vendorsetup.sh
@@ -0,0 +1,28 @@
+rm -rf kernel/asus/sdm660
+git clone --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm660 -b 4.4 kernel/asus/sdm660
+#rm -rf kernel/asus/sdm660/KernelSU/userspace
+#rm -rf kernel/asus/sdm660/KernelSU/website
+#rm -rf kernel/asus/sdm660/KernelSU/scripts
+#rm -rf kernel/asus/sdm660/KernelSU/docs
+
+rm -rf device/asus/sdm660-common
+rm -rf device/asus/X00TD
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common-4.4 -b main device/asus/sdm660-common
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_X00TD-4.4 -b main device/asus/X00TD
+
+#rm -rf hardware/qcom-caf/msm8998/audio
+#rm -rf hardware/qcom-caf/msm8998/display
+#rm -rf hardware/qcom-caf/msm8998/media
+#git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_audio -b 14 hardware/qcom-caf/msm8998/audio
+#git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_display -b 14 hardware/qcom-caf/msm8998/display
+#git clone --depth=1 https://github.com/inexdroid/hardware_qcom-caf_msm8998_media -b 14 hardware/qcom-caf/msm8998/media
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/Tiktodz/android_vendor_asus-4.4 -b main vendor/asus
+
+rm -rf vendor/lineage-priv/keys
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/inexdroid/keys keypub && cp -R keypub/* vendor/lineage-priv/keys/ && rm -rf keypub
+
+export KBUILD_BUILD_USER=queen
+export TZ=Asia/Jakarta
