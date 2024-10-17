24642411481401137495925b1b4ca4440404492d
Meisie Widuri <ewprjkt@proton.me>
2024-10-18 02:25:05 +0700

    X00TD: Sync extract scripts with templates

--- extract-files.sh
+++ extract-files.sh
@@ -14,20 +14,29 @@ function blob_fixup() {
 
     # Fix jar path
     product/etc/permissions/qti_fingerprint_interface.xml)
+        [ "$2" = "" ] && return 0
         sed -i 's|/system/framework/|/system/product/framework/|g' "${2}"
         ;;
 
     # remove android.hidl.base dependency
     vendor/lib/hw/camera.sdm660.so)
+        [ "$2" = "" ] && return 0
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
 
     # fingerprint: use libhidlbase-v32 for goodix
     vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so | vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+        [ "$2" = "" ] && return 0
         grep -q "libhidlbase-v32.so" "${2}" || "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
         ;;
-
+        *)
+            return 1
+            ;;
     esac
+    return 0
+}
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -39,5 +48,6 @@ set -e
 export DEVICE=X00TD
 export DEVICE_COMMON=sdm660-common
 export VENDOR=asus
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
