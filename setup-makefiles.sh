f65e8811d7e62490fabbb68c8d0d17335b18e946
Meisie Widuri <ewprjkt@proton.me>
2024-10-18 02:29:18 +0700

    X00TD: Set VENDOR_COMMON

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,5 +17,6 @@ set -e
 export DEVICE=X00TD
 export DEVICE_COMMON=sdm660-common
 export VENDOR=asus
+export VENDOR_COMMON=${VENDOR}
 
 "./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
