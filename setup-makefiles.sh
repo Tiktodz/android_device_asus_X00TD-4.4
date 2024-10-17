8d0dad7a68693a2989bf31383bb234f7a0ab9c43
Meisie Widuri <ewprjkt@proton.me>
2024-10-18 02:27:24 +0700

    X00TD: Sync extract scripts with templates II

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -6,10 +6,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
 export DEVICE=X00TD
 export DEVICE_COMMON=sdm660-common
 export VENDOR=asus
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
